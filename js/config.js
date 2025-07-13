ef87551e5739a5d386da05ee444329d57aa56cbe
yrj007 <143408793+yrj007@users.noreply.github.com>
2025-07-14 00:14:46 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_Rh4AZ4JQH', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1l7v5iuikdgbhlmdqch33l94f4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_qm2aNNFG8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7bl1872r5t3dicr2ackr42cae3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
